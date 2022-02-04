7a3c00292c5b412d7b11963eaa42327d29253a11
Almadavic <almadavic@live.com>
2022-02-04 14:04:51 -0300

    Versão Única

---
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -0,0 +1,32 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+	@EmbeddedId
+	private CategoriaId id;
+	
+	public Categoria() {
+	}
+	
+	public Categoria(String nome,String tipo) {
+		this.id = new CategoriaId(nome,tipo);
+	}
+
+	public String getNome() {
+		return id.getNome();
+	}
+	
+
+	public String getTipo() {
+		return id.getTipo();
+	}
+
+	
+	
+
+}
